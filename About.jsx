a4dd4fa17f26bf7f6b3d50b2e04f48e9a7427da0
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-09 12:14:00 +0330

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>درباره</h2>
+      <p>این پروژه به‌صورت آموزشی ساخته شده و سعی شده است ظاهری گرم و غیر ماشینی داشته باشد.</p>
+    </div>
+  )
+}
